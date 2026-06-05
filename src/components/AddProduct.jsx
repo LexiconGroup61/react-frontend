abb3630646f6617a81f69357f0d0dca2d06c43fe
tobiasengberg <tobias.engberg@exilic.se>
2026-06-05 08:48:26 +0200

    Merge conflict

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -25,32 +25,32 @@ const AddProduct = () => {
         <div className="bg-slate-200 p-5">
             <form className="flex flex-row" onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col">
-                <label>Name:</label>
-                <input className="border m-1" type="text" {...register("name")} />
+                    <label>Name:</label>
+                    <input className="border m-1" type="text" {...register("name")} />
                     <p>{errors.name?.message}</p>
                 </div>
                 <div className="flex flex-col">
-                <label>Description:</label>
-                <input className="border m-1" type="text" {...register("description", { required: true})} />
-                {errors.description && <p>This field is required</p>}
+                    <label>Description:</label>
+                    <input className="border m-1" type="text" {...register("description", { required: true})} />
+                    {errors.description && <p>This field is required</p>}
                 </div>
-                    <div className="flex flex-col">
-                        <label>Price:</label>
-                <input className="border m-1" type="number" {...register("price", {min: 0})} />
-                        {errors.price && <p>Price should be positive</p>}
-                        <p>{errors.price?.message}</p>
-                    </div>
-                        <div className="flex flex-col">
-                        <label>Category:</label>
-                <select className="border m-1" {...register("category")}>
-                    <option value="candy" />
-                    <option value="fruit" />
-                </select>
-                        </div>
-                            <div className="flex flex-col">
-                            <label>Src:</label>
-                <input className="border m-1" type="text" {...register("src")} />
-                            </div>
+                <div className="flex flex-col">
+                    <label>Price:</label>
+                    <input className="border m-1" type="number" {...register("price", {min: 0})} />
+                    {errors.price && <p>Price should be positive</p>}
+                    <p>{errors.price?.message}</p>
+                </div>
+                <div className="flex flex-col">
+                    <label>Category:</label>
+                    <select className="border m-1" {...register("category")}>
+                        <option value="candy" />
+                        <option value="fruit" />
+                    </select>
+                </div>
+                <div className="flex flex-col">
+                    <label>Src:</label>
+                    <input className="border m-1" type="text" {...register("src")} />
+                </div>
                 <input type="submit" value="Register new product"/>
             </form>
             
