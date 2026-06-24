775b575697c51d2ec82daf3438d7c51a996f2773
tobiasengberg <tobias.engberg@exilic.se>
2026-06-24 18:42:11 +0200

    Table filtering

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,7 +1,7 @@
 import {
     createColumnHelper,
     flexRender,
-    getCoreRowModel,
+    getCoreRowModel, getFilteredRowModel,
     getPaginationRowModel,
     getSortedRowModel,
     useReactTable
@@ -20,16 +20,16 @@ const data = [
     { id: 1, title: "First item", year: 1996 },
     { id: 2, title: "Second item", year: 2008 },
     { id: 3, title: "Third item", year: 1984 },
-    { id: 4, title: "Fourth item", year: 1984 },
-    { id: 5, title: "First item", year: 1996 },
+    { id: 4, title: "Fourth item", year: 2006 },
+    { id: 5, title: "First item", year: 1945 },
     { id: 6, title: "Second item", year: 2008 },
     { id: 7, title: "Third item", year: 1984 },
-    { id: 8, title: "Fourth item", year: 1984 },
+    { id: 8, title: "Fourth item", year: 1964 },
     { id: 9, title: "Fourth item", year: 1984 },
     { id: 10, title: "First item", year: 1996 },
     { id: 11, title: "Second item", year: 2008 },
-    { id: 12, title: "Third item", year: 1984 },
-    { id: 13, title: "Fourth item", year: 1984 }
+    { id: 12, title: "Third item", year: 2010 },
+    { id: 13, title: "Fourth item", year: 2025 }
 ]
 
 const columnHelper = createColumnHelper();
@@ -46,6 +46,7 @@ const columns = [
     columnHelper.accessor('year', {
         header: "Year",
         cell: info => info.getValue()
+
     })
 ]
 
@@ -57,22 +58,36 @@ const DataTable = () => {
         getCoreRowModel: getCoreRowModel(),
         getSortedRowModel: getSortedRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        globalFilterFn: "text",
         initialState: {
             pagination: {
-                pageSize: 5,
+                pageSize: 20,
             }
         }
     })
 
     return (
-            <Table>
-                <p>{table.getRowCount()} posts in total</p>
+        <>
+            <p>{table.getRowCount()} posts in total</p>
+            <input value=" "
+                   onChange={e => table.setGlobalFilter(String(e.target.value))}
+                   placeholder="Search"
+                   className="pl-6"
+            />
+            <Table className="m-auto">
+
                 <TableHeader>
                 { table.getHeaderGroups().map((headerGroup) => (
                     <TableRow>
                         {headerGroup.headers.map((header) => (
                             <TableHead className="text-right" onClick={header.column.getToggleSortingHandler()}>
                                 { flexRender(header.column.columnDef.header, header.getContext())}
+                                <input value={header.column.getFilterValue()}
+                                       onChange={e => header.column.setFilterValue(String(e.target.value))}
+                                       placeholder="Search"
+                                       className="pl-6"
+                                />
                             </TableHead>
                         ))}
                     </TableRow>
@@ -88,13 +103,15 @@ const DataTable = () => {
                     ))}
                 </TableRow>))}
                 </TableBody>
-                <button onClick={() => table.nextPage()}>Next page</button>
-                <button onClick={() => table.previousPage()}>Previous page</button>
-                <button onClick={() => table.firstPage()}>First page</button>
-                <button onClick={() => table.lastPage()}>Last page</button>
-                <p>{table.getPageCount()}</p>
-
+                <div className="flex justify-center w-100">
+                <button  className="px-10" onClick={() => table.nextPage()}>Next page</button>
+                <button className="px-10"  onClick={() => table.previousPage()}>Previous page</button>
+                <button className="px-10"  onClick={() => table.firstPage()}>First page</button>
+                <button className="px-10"  onClick={() => table.lastPage()}>Last page</button>
+                <p className="py-12">Total pages: {table.getPageCount()}</p>
+                </div>
             </Table>
+        </>
 
     );
 };
